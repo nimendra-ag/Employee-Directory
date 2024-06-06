6afdb8a3f70040191912be4b4c4cfac01e750f24
nimendra-ag <nimendraamiththa1@gmail.com>
2024-06-06 20:43:55 +0530

    this is the first commit

---
+++ src/main/java/com/ruhuna/rest/EmployeeRestController.java
@@ -0,0 +1,53 @@
+package com.ruhuna.rest;
+
+import com.ruhuna.model.Employee;
+import com.ruhuna.service.EmployeeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+public class EmployeeRestController {
+
+    private EmployeeService employeeService;
+
+    @Autowired
+    public EmployeeRestController(EmployeeService employeeService) {
+        this.employeeService = employeeService;
+    }
+
+    @GetMapping("/api/employees")
+    public ResponseEntity<List<Employee>> getAllEmployees()
+    {
+        List<Employee> employeeList = employeeService.findAllEmployees();
+
+        return new ResponseEntity<>(employeeList, HttpStatus.OK);
+    }
+
+    @GetMapping("/api/employees/{id}")
+    public ResponseEntity<Employee> getEmployeeById(@PathVariable String id)
+    {
+        Employee employee = employeeService.findEmployeeById(id);
+
+        if(employee == null)
+        {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+
+        return new ResponseEntity<>(employee, HttpStatus.OK);
+    }
+
+    @PostMapping("/api/employees")
+    public ResponseEntity<String> addNewEmployee(@RequestBody Employee employee){
+
+        employeeService.saveEmployee(employee);
+        return new ResponseEntity<>("Employee Saved Successfully", HttpStatus.CREATED);
+    }
+
+
+
+}
---
+++ src/main/java/com/ruhuna/service/EmployeeService.java
@@ -0,0 +1,12 @@
+package com.ruhuna.service;
+
+import com.ruhuna.model.Employee;
+
+import java.util.List;
+
+public interface EmployeeService {
+
+    List<Employee> findAllEmployees();
+    Employee findEmployeeById(String id);
+    void saveEmployee(Employee employee);
+}
